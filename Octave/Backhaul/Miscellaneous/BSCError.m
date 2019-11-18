ab8ee0be17e273b874f178bbab44e337b9578ecd
Sudhanshu Sharma <sharmasudhanshu2110@gmail.com>
2019-11-18 14:57:48 +0530

    Update

---
+++ Octave/Backhaul/Miscellaneous/BSCError.m
@@ -0,0 +1,13 @@
+%function to perform channel encoding 
+function [Chp, ChO] = ChannelError(P,ChR,encoded_bits) 
+  for i=1:length(ChR)
+    if (ChR(i)<(1-P))
+      ChR(i)=0;
+    else
+      ChR(i)=1;
+    endif
+  end
+Chp = ChR; % Generated channel Probability
+% XORing Channel Probability woth Encoded Bits 
+ChO = bitxor(ChR,encoded_bits);
+end
