d5bb328207e19ce593a9a0aed3de5079b5f485f3
Sudhanshu Sharma <sharmasudhanshu2110@gmail.com>
2020-01-17 11:45:50 +0530

    Update

--- Matlab Working/GPS/GANA/gpsRx.m
+++ Matlab Working/GPS/GANA/gpsRx.m
@@ -20,50 +20,51 @@ close all;
 [txSignal, payload, hFilt, codeOffsetArray, freqOffsetArray,symbol] = gpsTx();
 
 %% Adding Noise
-snr = -23;
+snr = -20;
 txSignal = awgn(txSignal,snr);
 
 %% Reciever
 codeLen = 1023;
 OSR = 10;
-numBits = 100;
+numBits = 10;
 Fs = OSR*1e6;
 J = sqrt(-1);
 hFiltLen = length(hFilt);
 [numSVs,lengthtx] =size(txSignal);
+numSVs =1;       % to make it run ones
 RxData = zeros(numSVs,lengthtx/(1023*OSR));
 % crossCorrData = zeros((lengthtx/10)+60,numSVs);
-% for nSV = 1:numSVs
-%     temp1 = txSignal(nSV,:);
-%     tx = temp1.*exp(J*(-1)*2*pi*freqOffsetArray(nSV)/Fs*[0:lengthtx-1]);
-%     tx = real(tx);
-%     downsamtx = downsample(tx,OSR);
-%     downsamtx_hfilt = conv(downsamtx,hFilt);
-%     downsamtx_hfilt = downsamtx_hfilt((hFiltLen-1)/2+1:end-(hFiltLen-1)/2);
-%     crossCorr = xcorr2(downsamtx_hfilt,symbol(nSV,:));
-%     outputData = crossCorr(1023:codeLen:end)/codeLen;
-%     RxData(nSV,:) = outputData;
-% end
-% 
-% %% Plotting avg value
-% avgValue = zeros(1,10);
-% 
-% for nSV = 1:numSVs
-%     val = 0;
-%     lastval = 0;
-%     for i =1:20:length(outputData)
-%         Data = RxData(nSV,:);
-%         lastval = lastval+20;
-%         avg = sum(Data(i:1:lastval))/20;
-%         val = val +1;
-%         avgValue(val)=avg;
-%     end
-%     output = (1*(avgValue >0));
-%     error = sum(bitxor(output,payload));
-%     disp (['Error for SNR ',num2str(snr),' in SV ',num2str(nSV),'  is ', num2str(error), ' in time domain']);
-% end
+for nSV = 1:numSVs
+    temp1 = txSignal(nSV,:);
+    tx = temp1.*exp(J*(-1)*2*pi*freqOffsetArray(nSV)/Fs*[0:lengthtx-1]);
+    tx = real(tx);
+    downsamtx = downsample(tx,OSR);
+    downsamtx_hfilt = conv(downsamtx,hFilt);
+    downsamtx_hfilt = downsamtx_hfilt((hFiltLen-1)/2+1:end-(hFiltLen-1)/2);
+    crossCorr = xcorr2(downsamtx_hfilt,symbol(nSV,:));
+    outputData = crossCorr(1023:codeLen:end)/codeLen;
+    RxData(nSV,:) = outputData;
+end
+
+%% Plotting avg value
+avgValuetime = zeros(1,numBits);
+
+for nSV = 1:numSVs
+    val = 0;
+    lastval = 0;
+    for i =1:20:length(outputData)
+        Data = RxData(nSV,:);
+        lastval = lastval+20;
+        avg = sum(Data(i:1:lastval))/20;
+        val = val +1;
+        avgValuetime(val)=avg;
+    end
+    outputtime = (1*(avgValuetime >0));
+    error = sum(bitxor(outputtime,payload));
+    disp (['Error for SNR ',num2str(snr),' in SV ',num2str(nSV),'  is ', num2str(error), ' in time domain']);
+end
 %% Frequency Domain
-FrequencyDomain=zeros(numSVs,2000);
+FrequencyDomain=zeros(numSVs,lengthtx/(1023*OSR));
 for nSV = 1:numSVs
     temp1 = txSignal(nSV,:);
     tx = temp1.*exp(J*(-1)*2*pi*freqOffsetArray(nSV)/Fs*[0:lengthtx-1]);
@@ -80,24 +81,26 @@ for nSV = 1:numSVs
         starting = starting +1023;
         FrequencyDomain(nSV,i) =crossCorr_FFT(1);
         i=i+1;
+        
     end
 end
-avgValue = zeros(1,numBits);
+avgValuefreq = zeros(1,numBits);
 freq = 20;                                %50Hz 
 for nSV = 1:numSVs
     val = 0;
     lastval = 0;
     for i =1:freq:length(FrequencyDomain)
-        Data = FrequencyDomain(nSV,:);
+        DataFreq = FrequencyDomain(nSV,:);
         lastval = lastval+freq;
-        avg = sum(Data(i:1:lastval))/freq;
+        avg = sum(DataFreq(i:1:lastval))/freq;
         val = val +1;
-        avgValue(val)=avg;
+        avgValuefreq(val)=avg;
     end
-    output = (1*(avgValue >0));
-    error = sum(bitxor(output,payload));
+    outputfreq = (1*(avgValuefreq >0));
+    error = sum(bitxor(outputfreq,payload));
     disp (['Error for SNR ',num2str(snr),' in SV ',num2str(nSV),'  is ', num2str(error), ' in Frequency Domain']);
 end 
-
+plot(RxData,'b');
+hold on; plot(FrequencyDomain/1023,'g');
 
 toc;
