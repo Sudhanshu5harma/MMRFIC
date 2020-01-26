14156c5806ff57c5f3a8241cb1a064f3b00d64fc
Sudhanshu Sharma <sharmasudhanshu2110@gmail.com>
2020-01-26 21:19:29 +0530

    update

--- Matlab Working/GPS/Current/gpsRx_ver2.m
+++ Matlab Working/GPS/Current/gpsRx_ver2.m
@@ -9,6 +9,7 @@
 %% ver  0.2   17-Jan-2020   Sudhanshu             Codeoffset and Frequency Offset
 %% ver  0.1   14-Jan-2020   Sudhanshu             Time and Frequency Domain
 %% ver  0.1   10-Jan-2020   Sudhanshu             Created
+%% changes - Zero offset,
 tic;
 clc;
 clear all;
@@ -18,7 +19,7 @@ close all;
 
 snr = 0;
 txSignal = awgn(txSignal,snr);
-
+% rxSignal = 10^(snr/20) * sum(txSignal,2) + randn(length(txSignal),1);
 %% Reciever
 
 codeLen = 1023;
@@ -38,27 +39,22 @@ for nSV = 1:allSVs
     init_g2 = ones(1,10);
     fbMode = ['SV',num2str(nSV)];
     [code, symbol] = GPS_GoldSequence_generator(init_g1, init_g2, codeLen, fbMode, 0);
-    refCode(:,nSV) = reshape(repmat(symbol, OSR, 1),OSR*CODE_LEN,1);
+    refCode(:,nSV) = reshape(repmat(symbol, OSR, 1),OSR*codeLen,1);
 end
 disp('Goldcode Generated and upsampled by 10');
 
 %% Calculating goldcode used
-%offsetMat = zeros(allSVs,codeLen*10);
+GoldCodeUsed = zeros(length(refCode)+(codeLen*10)-1,allSVs);
 endvalue = 10230;
+for CurrSV =1:numSVs
 for nSV =1:allSVs
-for kk = 1:length(dopplerFreqArray)  % +ve freq offset
-            freqOffset1 = dopplerFreqArray(kk);
-            disp(['Computing correlation for freq :',num2str(freqOffset1)]);
-
-            data1 = data .* exp(-J*2*pi*freqOffset1/Fs*[0:length(data)-1]');
-            %corrFFT = (temp .* conj(codeFFT));
-            corr1 = conv(data1,flipud(code));
-            %corr2 = conv(data1,(code));
-            %plot(abs(corr1));hold on; plot(abs(corr2),'r-.');
-            corr1 = corr1(5115:end-5115);
-            corr(:,ii,kk) = (sum((reshape(corr1,CODE_LEN*OSR,NUM_RPTS*NUM_BITS_AVG)),2));
+    corr = xcorr(txSignal(1:endvalue,CurrSV),refCode(:,nSV));
+    GoldCodeUsed(:,nSV) = corr;
+    [tempMax,maxIndex] = max(abs(GoldCodeUsed));
 end
+[maxPeak(CurrSV), goldval(CurrSV)] = max(tempMax);
 end
+ 
 %% Frequency Domain
 
 % FrequencyDomain=zeros(numSVs,lengthtx/(1023*OSR));
@@ -78,11 +74,11 @@ end
 %         starting = starting +1023;
 %         FrequencyDomain(nSV,i) =crossCorr_FFT(1);
 %         i=i+1;
-%         
+%
 %     end
 % end
 % avgValuefreq = zeros(1,numBits);
-% freq = 20;                                %50Hz 
+% freq = 20;                                %50Hz
 % for nSV = 1:numSVs
 %     val = 0;
 %     lastval = 0;
@@ -96,5 +92,5 @@ end
 %     outputfreq = (1*(avgValuefreq >0));
 %     error = sum(bitxor(outputfreq,payload));
 %     disp (['Error for SNR ',num2str(snr),' in SV ',num2str(nSV),'  is ', num2str(error), ' in Frequency Domain']);
-% end 
+% end
 toc;
