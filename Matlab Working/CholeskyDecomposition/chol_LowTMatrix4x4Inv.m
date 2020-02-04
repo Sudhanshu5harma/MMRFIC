a874f68ad20c0d753f9c801473ba466c016b2915
Sudhanshu Sharma <sharmasudhanshu2110@gmail.com>
2020-02-04 17:50:57 +0530

    update

---
+++ Matlab Working/CholeskyDecomposition/chol_LowTMatrix4x4Inv.m
@@ -0,0 +1,23 @@
+%% Function to calculate the inverse of a lower triangular 4x4 matrix 
+
+function [matrixInverse] = chol_LowTMatrix4x4Inv(A)
+
+determinant = A(1,1)*A(2,2)*A(3,3)*A(4,4); 
+determinant_fp = quantize1(determinant,20,16,'signed','round');
+adj(1,1) = A(2,2)*A(3,3)*A(4,4);
+
+adj(2,1) = -A(2,1)*A(3,3)*A(4,4);
+adj(2,2) = A(1,1)*A(3,3)*A(4,4);
+
+adj(3,1) = A(2,1)*A(3,2)*A(4,4)- A(2,2)*A(3,1)*A(4,4) ;
+adj(3,2) = -A(1,1)* A(3,2)*A(4,4)+ A(1,2)*A(3,1)*A(4,4) + A(1,1)*A(3,4)*A(4,2);
+adj(3,3) = A(1,1)*A(2,2)*A(4,4);
+
+adj(4,1) = -A(2,1)*A(3,2)*A(4,3) - A(2,2)*A(3,3)*A(4,1)  + A(2,2)*A(3,1)*A(4,3) + A(2,1)*A(3,3)*A(4,2);
+adj(4,2) = A(1,1)*A(3,2)*A(4,3)- A(1,1)*A(3,3)*A(4,2);
+adj(4,3) = -A(1,1)*A(2,2)*A(4,3);
+adj(4,4) = A(1,1)*A(2,2)*A(3,3);
+adj_fp = quantize1(adj,32,16,'signed','round');
+
+matrixInverse = adj_fp/determinant_fp;
+end 
--- Matlab Working/CholeskyDecomposition/test_linEqns_4x4.m
+++ Matlab Working/CholeskyDecomposition/test_linEqns_4x4.m
@@ -20,15 +20,15 @@ A1 = transpose(A)*A;
 ni = ceil(log2(max(max(abs(A1)))));    %Number of integer bits
 nt = 40;                              %Total number of bits;
 %%[L,D,L_Transpose] = chol_fp(A1,nt,ni,signmode,roundmode);
-[L,D,L_Transpose] = chol_float(A1)
+[L,D,L_Transpose] = chol_float(A1);
 
-% L_inv = chol4x4Inv(L);
-% D_inv = chol4x4Inv(D);
- L1_inv = Inverse(L);
- D1_inv = Inverse(D);
-L_Transpose_inv = L1_inv';
+ L_inv = chol_LowTMatrix4x4Inv(L);
+ D_inv = chol_LowTMatrix4x4Inv(D);
+%  L1_inv = Inverse(L);
+%  D1_inv = Inverse(D);
+L_Transpose_inv = L_inv';
 
-X = (L_Transpose_inv*D1_inv *L1_inv) * (transpose(A)*b);
+X = (L_Transpose_inv*D_inv *L_inv) * (transpose(A)*b);
 X_inBuilt = A\b;
 [X X_inBuilt]
 
