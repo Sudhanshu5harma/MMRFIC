5e80287a65f44b9ff079671f63f37ee91b07b03e
Sudhanshu Sharma <sharmasudhanshu2110@gmail.com>
2020-02-06 12:10:13 +0530

    update

--- Matlab Working/CholeskyDecomposition/test_linEqns_4x4.m
+++ Matlab Working/CholeskyDecomposition/test_linEqns_4x4.m
@@ -12,18 +12,19 @@ b = [5;4;8;9];
 
 
 signmode = 'signed';
-roundmode = 'trunc';
+roundmode = 'round';
 
 
 A1 = transpose(A)*A;
 
-ni = ceil(log2(max(max(abs(A1)))));    %Number of integer bits
-nt = 40;                              %Total number of bits;
+%ni = ceil(log2(max(max(abs(A1)))));    %Number of integer bits
+nt = 32;                              %Total number of bits;
 %%[L,D,L_Transpose] = chol_fp(A1,nt,ni,signmode,roundmode);
 [L,D,L_Transpose] = chol_float(A1);
-
- L_inv = chol_LowTMatrix4x4Inv(L);
- D_inv = chol_LowTMatrix4x4Inv(D);
+ni =ceil(log2(max(max(abs(L)))));
+L_inv = chol_LowTMatrix4x4Inv_fp(L,nt,ni,signmode,roundmode);
+ni =ceil(log2(max(max(abs(D)))));
+D_inv = chol_LowTMatrix4x4Inv_fp(D,nt,ni,signmode,roundmode);
 %  L1_inv = Inverse(L);
 %  D1_inv = Inverse(D);
 L_Transpose_inv = L_inv';
